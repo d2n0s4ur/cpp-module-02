2af8fdc50eb7812b476ef55e189c4b38ca8e2e18
d2n0s4ur <njh0625@gmail.com>
2023-01-15 17:53:47 +0900

    update: ex00

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnoh <jnoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 17:45:00 by jnoh              #+#    #+#             */
+/*   Updated: 2023/01/15 17:47:27 by jnoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class	Fixed
+{
+	private:
+		int					value_;
+		static const int	fractional_bits_ = 8;
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(const Fixed &fixed);
+		Fixed &operator= (const Fixed &fixed);
+
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnoh <jnoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/15 17:50:36 by jnoh              #+#    #+#             */
+/*   Updated: 2023/01/15 17:50:46 by jnoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int    main( void ) {
+
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
